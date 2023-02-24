933d83995ee273aa5e46824f3d11b525362dcabb
williamProgrammerar <william@johnston.se>
2023-02-24 22:47:46 +0100

    decreased amount of trash spawned and also added weight

--- Assets/Scripts/PathGenerator.cs
+++ Assets/Scripts/PathGenerator.cs
@@ -1,13 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Linq;
 
 public class PathGenerator : MonoBehaviour
 {
     public Transform[] startingPositions; // Possible starting positions for path generation
     public Transform[] extremePoints; // max min values for x and y
     private float minX, maxX, minY, maxY; 
+    
     public GameObject[] paths; // Array of objects that can be spawned on the 'path'
+    public int[] weights; 
+    private int totalWeight;
+
     public GameObject[] obstacles; // Array of obstacles that can be spawned where there is no path
 
     private List<GameObject> pathChildren = new List<GameObject>(); // List containing instanciated children
@@ -47,6 +52,8 @@ public class PathGenerator : MonoBehaviour
 
         // Random direction for the path generator
         direction = Random.Range(1, 7);
+
+        totalWeight = weights.Sum();
     }
 
     private void Update()
@@ -103,9 +110,21 @@ public class PathGenerator : MonoBehaviour
     }
 
     private void randomPathObject() {
+        int randN = Random.Range(0, totalWeight + 1);
+
+        int i = 0;
+        while (randN >= 0 && i < paths.Length) {
+            randN -=  weights[i];
+            i++;
+        }
+
+        GameObject pathChild = Instantiate(paths[i-1], transform.position, Quaternion.identity);
+        pathChildren.Add(pathChild);
+
+        /*
         int randPathObject = Random.Range(0, paths.Length);
         GameObject pathChild = Instantiate(paths[randPathObject], transform.position, Quaternion.identity);
-        pathChildren.Add(pathChild);
+        pathChildren.Add(pathChild);*/
     }
 }
 
